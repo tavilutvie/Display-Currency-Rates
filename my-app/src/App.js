c8eb15a36e64903d4554e2dbed8a90fc8663f1c4
Maula Izza Azizi <azizi.maula@gmail.com>
2022-09-15 22:10:18 +0700

    feat: add api

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
+import {useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({})
+  const [ratePost, setRatePost] = useState([])
+  // const [row, setRow] = undefined
+  // const app_key = process.env.REACT_APP_API_KEY;
+  // axios.get(`https://api.currencyfreaks.com/latest?apikey=5695d778bd9945928189edabda338cac`).then((res) => res.json).catch((err) => console.log(err))
+
+  const url = `https://api.currencyfreaks.com/latest?apikey=5695d778bd9945928189edabda338cac&symbols=CAD, IDR, JPY, CHF, EUR, GBP`;
+  useEffect(() => {
+    let fetchPost = async () => {
+      const response = await fetch(url);
+      const postData = await response.json();
+      setData(postData)
+      setRatePost(postData?.rates)
+    }
+  //  let row = axios.get(`https://api.currencyfreaks.com/latest?apikey=5695d778bd9945928189edabda338cac`).then((res) => res.json)
+    fetchPost();
+  }, [])
+
+  // console.log(data);
+  console.log(ratePost)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding: '20px'}}>
+      <h1>Tavi</h1>
+      <h2>{data?.date}</h2>
+      <h3>{data?.base}</h3>
+      {Object.keys(ratePost).map((item) => {
+        return <tr key={item}>
+          <td>{item}</td>
+          <td>{ratePost[item]}</td>
+        </tr>
+      })}
+  
+      {/* {
+        ratePost && ratePost?.map((post, index) => (
+          <div key={index}>
+            <li>{post?.rates}</li>
+          </div>
+        ))
+      } */}
     </div>
-  );
+  )
 }
 
 export default App;
